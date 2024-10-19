887dce786a3af9f744d04770ec4febb63683868a
galuh wikri <galuhwikri05@gmail.com>
2024-10-19 18:04:07 +0700

    Menyelesaikan View

--- MVC/app/views/templates/header.php
+++ MVC/app/views/templates/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Halaman  <?= $data['judul']; ?></title>
-    <link rel="stylesheet" href="http://prakweb_2024_C_223040147/MVC/public/css/bootrap.css">
+    <link rel="stylesheet" href="https://prakweb_2024_C_223040147/MVC/public/css/bootstrap.css">
 </head>
 <body>
