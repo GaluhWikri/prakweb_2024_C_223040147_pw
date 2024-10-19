48b22b74d7227780eebb38949e5851645530fb89
galuh wikri <galuhwikri05@gmail.com>
2024-10-19 17:56:51 +0700

    Menyelesaikan View

--- MVC/app/views/about/index.php
+++ MVC/app/views/about/index.php
@@ -1,3 +1,3 @@
 
     <h1>about me</h1>
-    <p>Halo nama saya <?=$data['nama'];?> umur saya <?=$data['umur'];?> tahun, say adalah seorang <?=$data['pekerjaan'];?> </p>
+    <p>Halo nama saya <?=$data['nama'];?> umur saya <?=$data['umur'];?> tahun, saya adalah seorang <?=$data['pekerjaan'];?> </p>
